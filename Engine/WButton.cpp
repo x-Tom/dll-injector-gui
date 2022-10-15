73578df3d55c8560f77054d8f152aa48a04e0c10
x-Tom <35583347+x-Tom@users.noreply.github.com>
2022-10-15 11:09:45 +0100

    g

--- Engine/WButton.cpp
+++ Engine/WButton.cpp
@@ -1,4 +1,10 @@
-WButton::WButton(HINSTANCE hInst, HMENU id, int x, int y, int w, int h) : GChild(hInst, WC_BUTTON, nullptr, id, x, y, w, h)
+WButton::WButton(HINSTANCE hInst, HMENU id, int x, int y, int w, int h, std::wstring txt, ) : GChild(hInst, WC_BUTTON, nullptr, id, x, y, w, h)
 {
+    
+}
+
+bool WButton::Create(HWND prnt){
+    GChild::Create(prnt);
+    hwnd = CreateWindowEx(0, WC_BUTTON, WS_CHILD | WS_VISIBLE | styles, x,y,w,h,prnt, id, hinst, nullptr)
     
 }
