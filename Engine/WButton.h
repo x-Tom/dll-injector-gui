c4f5c98a9be100ce04c88b23f5fac30bcc62be38
x-Tom <35583347+x-Tom@users.noreply.github.com>
2022-10-15 11:09:45 +0100

    g

--- Engine/WButton.h
+++ Engine/WButton.h
@@ -1,6 +1,6 @@
 class WButton : public GChild {
 public:
-    WButton(HINSTANCE, HMENU, int, int, int, int);
+    WButton(HINSTANCE, HMENU, int, int, int, int, std::wstring, LONG = BS_PUSHBUTTON);
 	WButton() = default;
 	~WButton() = default;
 	virtual bool Create(HWND) override;
@@ -8,6 +8,7 @@ public:
 	WButton& operator=(const WButton&) = delete;
 
 protected:
+	LONG styles;
+	std::wstring text;
 	virtual LRESULT WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
-
 }
